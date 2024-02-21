9a8a9edd508a536249dfb5f41dd31a7b2148739e
ammarali-dev <tabraiz.ammarali@gmail.com>
2024-02-21 12:10:57 +0500

    CSS comp.

--- Week1/HTML & CSS/CODE/script.js
+++ Week1/HTML & CSS/CODE/script.js
@@ -1,22 +1,30 @@
-const headerNavButton = document.querySelector('#nav-icon');
-const headerNavButton = document.querySelector('#nav-icon');
+const menuContainer= document.querySelector('.menu-container');
+const NavIcon = document.querySelector('.nav-icon');
+const MenuCloseIcon = document.querySelector('.menu-close-icon');
+const menuOverlay = document.querySelector('.menu-overlay');
+
 let showMenu = false;
 
-headerNavButton.addEventListener('click',toggleMenu);
+console.log(NavIcon);
+console.log(menuContainer);
+NavIcon.addEventListener('click',MenuOn);
+MenuCloseIcon.addEventListener('click',MenuOff);
 
-function toggleMenu(){
-    if(!showMenu){
-        hamBurger.classList.add('open');
-        nav.classList.add('open');
-        menuNav.classList.add('open');
-        navItems.forEach(item=> item.classList.add('open'));
-        showMenu=true;
-    }
-    else{
-        hamBurger.classList.remove('open');
-        nav.classList.remove('open');
-        menuNav.classList.remove('open');
-        navItems.forEach(item=> item.classList.remove('open'));
-        showMenu=false;
-    }
+menuOverlay.addEventListener('click',MenuOff);
+
+function MenuOn(){
+        menuContainer.classList.remove('menu-close');
+        menuContainer.classList.add('menu-open');
+        menuOverlay.classList.remove('display-none');
+}
+function MenuOff(){
+
+   
+        menuContainer.classList.remove('menu-open');
+
+        menuContainer.classList.add('menu-close');
+        menuOverlay.classList.add('display-none');
+
+      
+       
 }
